c4b08addfe2160151985958c9d12781352113a30
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 13:06:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Calculate percentage with grade letter
+ * @author Gavin Sandhar
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner or user input
+    Scanner input = new Scanner(System.in);
+    
+    // ask user to input grade
+    System.out.println("Please enter a letter grade");
+    String grade = input.nextLine();
+
+    // which grade equals what percentage
+    switch(grade){
+      case "A":
+       System.out.println("This is between 80% and 100%");
+       break;
+      case "B":
+       System.out.println("This is between 70% and 79%");
+       break;
+      case "C":
+       System.out.println("This is between 60% and 69%");
+       break;
+      case "D":
+       System.out.println("This is between 50% and 59%");
+       break;
+      case "F":
+       System.out.println("This is between 0% and 49%");
+       break; 
+      default:
+      System.out.println("Invalid choice"); 
+    }
     
   }
 }
